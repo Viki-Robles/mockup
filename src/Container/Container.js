91eb4e0d3147304bf418ae9e1ed8d502faa3b5e2
Vicky Vasilopoulou <vickyvasilopoulou@Vickys-MacBook.local>
2020-07-31 20:36:47 +0300

    API updated Marshmallow

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -1,30 +1,39 @@
-import React from "react";
-import picturesmall from "./images/picturesmall.png";
-import mockuppicture from "./images/mockuppic.png";
-import "./Container.css";
+import React, { useEffect, useState } from "react";
+
+
 
 export default function Container() {
-    return (
-        <div className="container">
-            <div className="container-box-one">
-                <div className="container-box-one-big">
-                    <img src={picturesmall} alt="" className="small-picture" />
-                    <div className="container-box-text">
-                        <h2 className="container-box-title">GRIP</h2>
-                        <br/>
-                        <h2 className="container-box-title-second">VIRTUAL</h2>
-                    </div>
+    const [data, setData] = useState([]);
+    const [load, setLoad] = useState(false);
+
+    useEffect(() => {
+        fetch("https://api.spacexdata.com/v3/dragons")
+            .then((response) => response.json())
+            .then((data) => {
+                setData(data);
+                setLoad(true);
+            });
+    }, []);
+
+    if (!load) {
+        return <div>Loadin Main...</div>;
+    } else {
+        return (
+            <>
+                <div>
+                    {
+                        data.map((data) => <div key={data.id}>
+                            <div className="dragon-container">
+                                <div className="dragon-title">{data.name}</div>
+                                <div className="dragon-title"><img src={data.flickr_images[1]}/></div>
+                                <div className="dragon-type">{data.type}</div>
+                                <div className="dragon-description">{data.description}</div>
+                            </div>
+                        </div>
+                        )
+                    }
                 </div>
-                <div className="container-box-one-small">
-                    <h5>Welcome to Grip Virtual. Please log in below to get started.</h5>
-                    <button>Login</button>
-                </div>
-            </div>
-            <div className="container-box-two">
-                <img src={mockuppicture} alt="" className="big-picture" />
-                <h6>Take the work out of networking. Meet the right people for your business needs with our<br/>intelligent matchmaking technology.</h6>
-                <h5 className="container-second-box-title">Log in -70% od thiose who did said that they made a connection they would not <br/>have made otherwise.</h5>
-            </div>
-        </div>
-    );
+            </>
+        )
+    }
 }
